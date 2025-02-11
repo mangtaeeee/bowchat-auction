b249a7794502f7b04e4e241678efd7eecfa32903
mangtaeeee <osp9658@naver.com>
2025-02-11 20:26:45 +0900

    feat : firstCommit

---
+++ src/main/java/com/example/bowchat/BowchatApplication.java
@@ -0,0 +1,13 @@
+package com.example.bowchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bowchat/BowchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bowchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
