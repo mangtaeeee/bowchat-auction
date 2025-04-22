b998964745f8dd4fa5a11feb90059e7207f39215
mangtaeeee <osp9658@naver.com>
2025-04-22 15:02:53 +0900

    refactor: OAuth2.0 관련 설정 추가

---
+++ src/main/java/com/example/bowchat/user/entity/User.java
@@ -0,0 +1,41 @@
+package com.example.bowchat.user.entity;
+
+
+import com.example.bowchat.chatroom.entity.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "USERS")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    // 일반 로그인용. OAuth2 로그인은 null일 수 있음
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    // 로그인 제공자: local, google 등
+    @Column(nullable = false)
+    private String provider;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+}
