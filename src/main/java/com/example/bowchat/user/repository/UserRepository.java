9ea680e8354d5aebedc43547c7746039aaa95596
mangtaeeee <osp9658@gmail.com>
2025-07-22 14:42:28 +0900

    refactor : 응답 필드 수정

--- src/main/java/com/example/bowchat/user/repository/UserRepository.java
+++ src/main/java/com/example/bowchat/user/repository/UserRepository.java
@@ -4,14 +4,10 @@ import com.example.bowchat.user.entity.ProviderType;
 import com.example.bowchat.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Collection;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
     Optional<User> findByEmailAndProvider(String email, ProviderType provider);
-
-    boolean existsByIdIn(Collection<Long> id);
-
 }
